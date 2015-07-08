de8e6cf9802932bf12af46b6f46f9bb27e3b5492
James Maddison <james@rarge.co.uk>
2015-07-08 22:46:29 +0100

    spelt colour 'wrong' in include guard

--- src/imgen/color.hpp
+++ src/imgen/color.hpp
@@ -1,12 +1,14 @@
-#ifndef IMGEN_COLOUR_HPP_
-#define IMGEN_COLOUR_HPP_
+#ifndef IMGEN_COLOR_HPP_
+#define IMGEN_COLOR_HPP_
 
 #include <boost/gil/gil_all.hpp>
 
 namespace imgen {
     namespace gil = boost::gil;
 
+    /** Alias to the GIL pixel type imgen is using as it's colour type. */
     typedef gil::rgb8_pixel_t color;
+
 } // namespace imgen
 
-#endif // IMGEN_COLOUR_HPP_
+#endif // IMGEN_COLOR_HPP_
