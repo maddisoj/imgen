17821f99f1da5350307e351db965d20a06f0f1ca
James Maddison <james@rarge.co.uk>
2015-07-09 00:27:48 +0100

    added missing _t suffix

--- src/imgen/color.cpp
+++ src/imgen/color.cpp
@@ -1,11 +1,11 @@
 #include "imgen/color.hpp"
 
 namespace imgen {
-    color random_color() {
-        auto min = gil::channel_traits<channel>::min_value();
-        auto max = gil::channel_traits<channel>::max_value();
+    color_t random_color() {
+        auto min = gil::channel_traits<channel_t>::min_value();
+        auto max = gil::channel_traits<channel_t>::max_value();
 
-        return color(min + std::rand() % (max - min),
+        return color_t(min + std::rand() % (max - min),
                      min + std::rand() % (max - min),
                      min + std::rand() % (max - min));
     }
