44ab06cbf56d73844ee5a5ea6fd9bc5c5d670046
James Maddison <james@rarge.co.uk>
2015-07-16 19:41:10 +0100

    made tile pattern tile the whole area

--- patterns/tile.py
+++ patterns/tile.py
@@ -1,16 +1,19 @@
 import imgen
+import math
 
 
 class Pattern(imgen.Pattern):
     def draw(self, image, context, palette):
+        num_tiles = 10
         num_colors = len(palette.colors)
-        width = image.width() / num_colors
-        height = image.height()
-        x = 0
+        width = image.width() / num_tiles
+        height = image.height() / num_tiles
 
-        for color in palette.colors:
+        for tile in range(0, num_tiles ** 2):
+            x = max(0, width * (tile % num_tiles))
+            y = max(0, height * math.floor(tile / num_tiles))
+            color = palette.colors[tile % num_colors]
+
             context.set_color(color)
-            context.rectangle(x, 0.0, x + width, height)
+            context.rectangle(x, y, x + width, y + height)
             context.fill()
-
-            x += width
