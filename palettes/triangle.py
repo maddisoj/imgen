653482e8f3d79b332dff8085b2b4e2925bb0e1a2
James Maddison <james@rarge.co.uk>
2015-07-16 19:40:47 +0100

    clamped saturation and lightness

--- palettes/triangle.py
+++ palettes/triangle.py
@@ -1,22 +1,29 @@
 import imgen
 import math
+import random
 
 
 class Palette(imgen.Palette):
     def __init__(self):
         super().__init__()
 
-        base = imgen.random_hsl()
+        base = random.uniform(0.0, 1.0)
         hues = [
-            base[0],
-            base[0] + 0.33,
-            base[0] - 0.33
+            base,
+            base + 0.33,
+            base - 0.33
         ]
+        # Clamp lightness to [0.2, 0.8] to stop really dark or really light
+        # colours
+        lightness = random.uniform(0.2, 0.8)
+
+        # Clamp saturation to prevent washed out colours.
+        saturation = random.uniform(0.2, 0.8)
 
         for hue in hues:
             if hue < 0.0 or hue > 1.0:
                 hue -= math.floor(hue)
 
             self.colors.append(
-                imgen.hsl2rgb(imgen.hsl(hue, base[1], base[2]))
+                imgen.hsl2rgb(imgen.hsl(hue, saturation, lightness))
             )
