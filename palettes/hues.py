af10091e783e91c92551f63793d662fe3e7370a5
James Maddison <james@rarge.co.uk>
2015-08-05 16:49:23 +0100

    updated palettes to use new interface

--- palettes/hues.py
+++ palettes/hues.py
@@ -3,27 +3,25 @@ import math
 import random
 
 
-# Generates colours whose hues are distributed in 2/5 of the colour wheel. The
-# saturation and lightness are the same for each colour with both being clamped
-# to a subset to prevent murky colours.
+# Generates colours whose hues are evenly distributed around the colour wheel.
+# The saturation and lightness are the same for each colour with both being
+# clamped to a subset to prevent murky colours.
 class Palette(imgen.Palette):
     def generate(self, **kwargs):
         colors = []
         lower = kwargs['lower'] if 'lower' in kwargs else 1
         upper = kwargs['upper'] if 'upper' in kwargs else 10
         amount = kwargs['amount'] if 'amount' in kwargs else random.randint(lower, upper)
+        step = 1 / amount
         base = random.uniform(0.0, 1.0)
         lightness = random.uniform(0.2, 0.8)
-        saturation = random.uniform(0.4, 1.0)
-        step = 0.4 / amount
+        saturation = random.uniform(0.4, 0.8)
         hues = [base + i * step for i in range(amount)]
 
         for hue in hues:
             if hue < 0.0 or hue > 1.0:
                 hue -= math.floor(hue)
 
-            colors.append(
-                imgen.hsl2rgb(imgen.hsl(hue, lightness, saturation))
-            )
+            colors.append(imgen.hsl2rgb(imgen.hsl(hue, saturation, lightness)))
 
         return colors
--- palettes/shades.py
+++ palettes/shades.py
@@ -1,19 +1,30 @@
 import imgen
+import math
 import random
 
 
+# Generates colours whose lightnesses are evenly distributed around the colour
+# wheel. The hue and saturation remain the same for each colour. The
+# saturation and lightness are clamped to a subset to prevent murky colours
 class Palette(imgen.Palette):
-    def __init__(self):
-        super().__init__()
-
+    def generate(self, **kwargs):
+        colors = []
+        lower = kwargs['lower'] if 'lower' in kwargs else 3
+        upper = kwargs['upper'] if 'upper' in kwargs else 6
+        amount = kwargs['amount'] if 'amount' in kwargs else random.randint(lower, upper)
+        base = random.uniform(0.2, 0.8)
+        hue = random.uniform(0.0, 1.0)
+        saturation = random.uniform(0.4, 0.8)
         lightnesses = [
-            random.uniform(0.2, 0.8) for i in range(random.randint(3, 6))
+            base + (i / amount) for i in range(amount)
         ]
 
-        hue = random.uniform(0.0, 1.0)
-        saturation = random.uniform(0.4, 1.0)
-
         for lightness in lightnesses:
-            self.colors.append(
+            if lightness < 0.0 or lightness > 1.0:
+                lightness -= math.floor(lightness)
+
+            colors.append(
                 imgen.hsl2rgb(imgen.hsl(hue, saturation, lightness))
             )
+
+        return colors
